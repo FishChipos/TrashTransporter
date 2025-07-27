69a456286fd975be629a8bb943639dae87850f1e
Carlsson Khovis <Carlkho99@gmail.com>
2025-07-27 10:57:12 +0700

    Updated README.md and added basic GPS code.

--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
 
 #include <WiFi.h>
-#include <ESPmDNS.h>
-#include <ESP32PWM.h>
-#include <SerialTransfer.h>
+#include <ESPmDNS.h> // mDNS to broadcast API.
+#include <ESP32PWM.h> // Handle motors and servos.
+#include <SerialTransfer.h> // Handle transfer from ESP32-CAM.
+#include <SoftwareSerial.h> // Handle serial from GPS.
+#include <TinyGPSPlus.h> // Handle GPS data.
 
 #include "motor.hpp"
 #include "gripper.hpp"
@@ -13,8 +15,11 @@
 #define WIFI_SSID "CHANGE ME"
 #define WIFI_PASS "CHANGE ME"
 
-const int PIN_TX = 0;
-const int PIN_RX = 0;
+const int PIN_CAM_TX = 0;
+const int PIN_CAM_RX = 0;
+
+const int PIN_GPS_TX = 0;
+const int PIN_GPS_RX = 0;
 
 // Change as necessary.
 Motor leftMotor(22, 5, 18);
@@ -26,10 +31,13 @@ Gripper backGripper(15, 25);
 // Web server on port 80.
 ServerAPI server(80);
 
-SerialTransfer serial1Transfer;
+SerialTransfer camSerialTransfer;
 // Using QVGA, the size should never exceed 320 x 240 bytes with JPEG compression.
 uint8_t rxBuffer[320 * 240];
 int receiveSize = 0;
+
+SoftwareSerial gpsSoftwareSerial(PIN_GPS_RX, PIN_GPS_TX);
+TinyGPSPlus gps;
 
 void setup() {
     // Helps with timer stability when controlling servos.
@@ -39,8 +47,10 @@ void setup() {
     ESP32PWM::allocateTimer(3);
 
     Serial.begin(115200);
-    Serial1.begin(115200, SERIAL_8N1, PIN_RX, PIN_TX);
-    serial1Transfer.begin(Serial1);
+    Serial1.begin(115200, SERIAL_8N1, PIN_CAM_RX, PIN_CAM_TX);
+    camSerialTransfer.begin(Serial1);
+    // The NEO-6M GPS module works at 9600 baud.
+    gpsSoftwareSerial.begin(9600);
 
     delay(100);
 
@@ -73,10 +83,10 @@ void setup() {
 void loop() {
     server.handleClient();
 
-    // There is no way this works.
-    if (serial1Transfer.available()) {
-        if (serial1Transfer.currentPacketID() == 1) {
-            receiveSize = serial1Transfer.rxObj(rxBuffer, receiveSize, 320 * 240);
+    There is no way this works.
+    if (camSerialTransfer.available()) {
+        if (camSerialTransfer.currentPacketID() == 1) {
+            receiveSize = camSerialTransfer.rxObj(rxBuffer, receiveSize, 320 * 240);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,17 +1,40 @@
 # Trash Transporter
 
-## ESP32 Firmware
+## Hardware
+
+1. ESP32
+2. ESP32-CAM
+3. NEO-6M GPS module
+
+## ESP32 and ESP32-CAM Firmware
 
 ### Dependencies
 
 This project requires VSCode with the PlatformIO extension installed.
 
-The following libraries also need to be installed through PlatformIO and added to the project:
-1. Blynk
-2. ESP32Servo
+If the libraries were not installed automatically after setup, the following libraries can be installed and added manually.
+
+For ESP32:
+1. ESP32Servo
+2. SerialTransfer
+3. EspSoftwareSerial
+4. TinyGPSPlus-ESP32
+
+For ESP32-CAM:
+1. SerialTransfer
 
 ### Building
 
-Open the `ESP` folder with the PlatformIO extension and wait for it to finish configuring the project.
+Open the `ESP32` folder with the PlatformIO extension and wait for it to finish configuring the project. Do the same for the `ESP32-CAM` folder.
 
 Use the `Build` action to build the project and the `Upload` action to upload the firmware to a connected board.
+
+## Front-end
+
+### Dependencies
+
+The front-end requires node.js and npm.
+
+### Building
+
+To run the front-end, use `npm run dev` and follow the displayed link.
